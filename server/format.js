70d48ce593ba731807144111edb9ee7baa7759df
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-12-03 21:11:00 -0600

    made functions more declarative

--- server/format.js
+++ server/format.js
@@ -10,94 +10,128 @@ const format = {
 
     return month;
   },
+
   getGroups: function (data) {
     this.groups = this.uniqueIds(data, 'groupId');
-    this.get('groups', 'groupId', data);
+
+    this.create('groups').from(data).using('groupId');
     this.getItems(data);
     this.addProp('items').to('groups');
+
     return this.groups;
   },
+
   getItems: function (data) {
     this.items = this.uniqueIds(data, 'itemId');
-    this.get('items', 'itemId', data);
+
+    this.create('items').from(data).using('itemId');
     this.getSplits(data);
     this.addProp('splits').to('items');
   },
+
   getSplits: function (data) {
     this.splits = this.uniqueIds(data, 'splitId');
-    this.get('splits', 'splitId', data);
+
+    this.create('splits').from(data).using('splitId');
     this.getTransactions(data);
     this.addProp('transactions').to('splits');
   },
+
   getTransactions: function (data) {
     this.transactions = this.uniqueIds(data, 'transactionId');
-    this.get('transactions', 'transactionId', data);
+
+    this.create('transactions').from(data).using('transactionId');
     this.addProp('splits').to('transactions');
   },
-  addProp: propName => {
+
+  addProp: function (propName) {
     return {
-      to: parentName => {
+
+      to: function (parentName) {
         format[parentName].forEach(parentEl => {
           format[propName].forEach(propEl => {
-            format.createOn(parentName, propName, [parentEl, propEl]);
+            format.create(propName).on(parentName).using([parentEl, propEl]);
           });
         });
       }
     };
   },
-  createOn: function (parentName, propName, elements) {
-    let [parentEl, propEl] = elements;
-    let idRef = `${parentName}IdRef`;
 
-    if (parentName === 'splits') {
-      idRef = `${propName}IdRef`;
-      if (propEl.id === parentEl[idRef]) {
-        parentEl[propName] = propEl;
+  create: function (propName) {
+    return {
+
+      from: function (data) {
+        return {
+
+          using: function (id) {
+            data.forEach(dataObj => {
+              const index = format[propName].indexOf(dataObj[id]);
+              const inArray = index !== -1;
+              if (inArray) {
+                format.set[propName](index, dataObj);
+              }
+            });
+          }
+        };
+      },
+
+      on: function (parentName) {
+        return {
+
+          using: function (elements) {
+            let [parentEl, propEl] = elements;
+            let idRef = `${parentName}IdRef`;
+
+            if (parentName === 'splits') {
+              idRef = `${propName}IdRef`;
+
+              if (propEl.id === parentEl[idRef]) {
+                parentEl[propName] = propEl;
+              }
+            }
+
+            if (parentEl.id === propEl[idRef]) {
+              if (parentName === 'transactions') {
+                const { ...propCopy } = propEl;
+                propEl = propCopy;
+              }
+
+              if (!parentEl[propName]) {
+                parentEl[propName] = [];
+              }
+
+              parentEl[propName].push(propEl);
+            }
+          }
+        };
       }
-    }
-
-    if (parentEl.id === propEl[idRef]) {
-      if (!parentEl[propName]) {
-        parentEl[propName] = [];
-      }
-
-      if (parentName === 'transactions') {
-        const { ...propCopy } = propEl;
-        propEl = propCopy;
-      }
-
-      parentEl[propName].push(propEl);
-    }
-    // }
+    };
   },
-  get: function (property, id, data) {
-    data.forEach(dataObj => {
-      const index = this[property].indexOf(dataObj[id]);
-      const inArray = index !== -1;
-      if (inArray) {
-        this.set[property](index, dataObj, this);
-      }
-    });
-  },
+
   uniqueIds: function (data, type) {
     const idSet = new Set();
+
     data.forEach(dataObj => {
       idSet.add(dataObj[type]);
     });
+
     const idArr = [...idSet];
+
     return idArr;
   },
+
   set: {
-    groups: function (index, data, formatObj) {
-      formatObj.groups[index] = {
+    groups: function (index, data) {
+      format.groups[index] = {
         id: data.groupId,
         name: data.groupName,
         order: data.groupOrder,
         type: data.budgetType
       };
     },
-    items: function (index, data, formatObj) {
-      formatObj.items[index] = {
+
+    items: function (index, data) {
+      format.items[index] = {
         id: data.itemId,
         name: data.itemName,
         order: data.itemOrder,
@@ -108,16 +142,18 @@ const format = {
         groupsIdRef: data.groupIdRef
       };
     },
-    splits: function (index, data, formatObj) {
-      formatObj.splits[index] = {
+
+    splits: function (index, data) {
+      format.splits[index] = {
         id: data.splitId,
         amount: data.splitAmount,
         itemsIdRef: data.itemIdRef,
         transactionsIdRef: data.transactionIdRef
       };
     },
-    transactions: function (index, data, formatObj) {
-      formatObj.transactions[index] = {
+
+    transactions: function (index, data) {
+      format.transactions[index] = {
         id: data.transactionId,
         name: data.transactionName,
         type: data.transactionType,
