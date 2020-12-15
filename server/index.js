38dc3b36d4492749f9dca0a0381b5a6a8f6d54d8
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-12-15 12:04:16 -0600

    modified index comments

--- server/index.js
+++ server/index.js
@@ -7,37 +7,25 @@ const { get, post } = require('./routes');
 
 const app = express();
 
-/*
-* TOP LEVEL MIDDLEWARE
-*/
-
+// TOP LEVEL MIDDLEWARE
 app.use(staticMiddleware); // serves static files in the public path
 app.use(sessionMiddleware); // stores session data in session file
 app.use(express.json()); // parses request bodies as JSON
 
-/*
-* GET METHODS
-*/
-
+// GET METHODS
 get.healthCheck(app); // check if server can connect to database
 get.month(app); // retrieve budget by monthId
 
-/*
-* POST METHODS
-*/
-
+// POST METHODS
 post.group(app); // add a new group
 post.item(app); // add a new group item
 post.transaction(app); // add a new transaction
 
-/*
-* ERROR HANDLERS
-*/
-
+// ERROR HANDLERS
 handlePathError(app); // handles unhandled requests on paths with root "/api"
 sendError(app); // versitile error handling middleware
 
-// start server
+// START SERVER
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port', process.env.PORT);
